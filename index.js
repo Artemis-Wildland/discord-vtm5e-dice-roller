1eb8973c51d109538cbbe28aba6298b3351f71a2
Artemis Wildland <ashley.pyle@hotmail.co.uk>
2020-12-15 18:23:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -178,4 +178,9 @@ function generateRoll(blackDice, getHungryDice, receivedMessage, extractDiffcuit
     }
 }
 
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
+
 client.login(process.env.token) // Bot token
